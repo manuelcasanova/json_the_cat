c0fdf4854076b406c6b802e317ba675a9cdcc3d1
Manuel Casanova <manucasanova@hotmail.com>
2022-02-23 16:52:24 -0800

    LINTED and erased unused variables

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,8 +4,6 @@ const request = require('request');
 
 
 const search = `https://api.thecatapi.com/v1/breeds/search?q=${args[2]}`;
-const breedList = `https://api.thecatapi.com/v1/breeds`
-
 
 request(search, (error, response, body) => {
   if (error) {
@@ -13,11 +11,11 @@ request(search, (error, response, body) => {
     //console.log("Error: the request has failed");
   } else {
     if (body === "[]") {
-      console.log(`We could not find any information about the - ${args[2]} - breed`)
+      console.log(`We could not find any information about the - ${args[2]} - breed`);
     } else {
-    const data = JSON.parse(body); //convert to object
-    let description = data[0].description; //Because we accesing the first (and only in this case) entry of an array.
-    console.log(description);
+      const data = JSON.parse(body); //convert to object
+      let description = data[0].description; //Because we accesing the first (and only in this case) entry of an array.
+      console.log(description);
     }
   }
 });
