f6faed6bb68b2d75f8f05d6be3e8862bbdb7dd3a
Manuel Casanova <manucasanova@hotmail.com>
2022-02-23 16:48:20 -0800

    PRINTS A MESSAGE IF THE BREED IS NOT FOUND

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,19 +4,25 @@ const request = require('request');
 
 
 const search = `https://api.thecatapi.com/v1/breeds/search?q=${args[2]}`;
+const breedList = `https://api.thecatapi.com/v1/breeds`
 
 
 request(search, (error, response, body) => {
   if (error) {
-    //throw Error("The request has failed");
-    console.log("Error: the request has failed");
+    throw Error("The request has failed");
+    //console.log("Error: the request has failed");
   } else {
+    if (body === "[]") {
+      console.log("The breed does not exist")
+    } else {
     const data = JSON.parse(body); //convert to object
     let description = data[0].description; //Because we accesing the first (and only in this case) entry of an array.
     console.log(description);
+    }
   }
 });
 
-/*
-If data[0].description = ""
-*/
+
+
+
+
